fd507fa55e8921372fde534d90e9201501c19b42
Евгений <eugene.kurlenya@gmail.com>
2017-10-03 16:55:41 +0300

    Первые наработки.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+//module.exports = 
+function check(str, bracketsConfig) {
+  let commonBracketArray = [];
+  let sizeBracketConfig = bracketsConfig.length;
+  let sizeStr = str.length;
+  if(sizeStr % 2 != 0)
+    return false;
+  let count = 0;
+
+  for (let i = 0; i < sizeBracketConfig; i++) {
+    for (let j = 0; j < 2; j++) {
+      commonBracketArray.push(bracketsConfig[i][j]);
+    }
+  }
+
+
+  for (let i = 0; i < sizeStr; i++) {
+    if (count == -1)
+      break;
+    let a = 0;
+    checkOpenBracket(str[i], i, commonBracketArray, bracketsConfig);
+
+
+  }
+
+  if (count == 0)
+    return true;
+  else
+    return false;
+
 }
+
+console.log(check('([{}])', config3));
+
+function checkOpenBracket(symbol, pos, commonBracketArray, bracketsConfig) {
+
+  console.log(commonBracketArray.indexOf(symbol));
+
+}
+
