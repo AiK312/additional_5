85283817582c5a7b0942e4bb1a56a8ca4e46fdc2
Евгений <eugene.kurlenya@gmail.com>
2017-10-05 17:26:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
 
 //module.exports = 
 function check(str, bracketsConfig) {
-  let commonBracketArray = [];
-  let sizeBracketConfig = bracketsConfig.length;
-  let sizeStr = str.length;
-  if(sizeStr % 2 != 0)
+  let commonBracketArray = [];//array of bracketsConfig
+  let resultBracketArray = [];
+  let sizeBracketConfig = bracketsConfig.length;//size bracketsConfig
+  let sizeStr = str.length;//size input string
+  let count = 0;//main count. If value this count less zer0 => return false
+  let sameBrackets = { countSameSymbols: 0 };//object with property for same brackets and numbers
+  //check for odd size of string.
+  if (sizeStr % 2 != 0)
     return false;
-  let count = 0;
 
   for (let i = 0; i < sizeBracketConfig; i++) {
     for (let j = 0; j < 2; j++) {
@@ -21,28 +24,42 @@ function check(str, bracketsConfig) {
     }
   }
 
-
   for (let i = 0; i < sizeStr; i++) {
     if (count == -1)
-      break;
-    let a = 0;
-    checkOpenBracket(str[i], i, commonBracketArray, bracketsConfig);
-
+      return false;
+    if (checkOpenBracket(str[i], str[i + 1], commonBracketArray, sameBrackets)) {
+      resultBracketArray.push(str[i]);
+      count++;
+    }
+    else {
+      if ((commonBracketArray.indexOf(str[i]) - 1) == (commonBracketArray.indexOf(str[i - 1]))) {
+        resultBracketArray.pop();
+        count--;
+      } else {
+        return false;
+      }
+    }
 
   }
 
-  if (count == 0)
-    return true;
-  else
-    return false;
-
+  return true;
 }
 
-console.log(check('([{}])', config3));
+console.log(check('()||', config5));
 
-function checkOpenBracket(symbol, pos, commonBracketArray, bracketsConfig) {
-
+function checkOpenBracket(symbol, nextSymbol, commonBracketArray) {
   console.log(commonBracketArray.indexOf(symbol));
+  if (commonBracketArray.indexOf(symbol) % 2 == 0) {
+    if (true) {
+      console.log(symbol);
+    } else {
+      return true;
+    }
+  }
+  else {
+    return false;
+  }
+
 
 }
 
