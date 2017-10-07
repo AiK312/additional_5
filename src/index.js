1474e0a628609584abaf83e808a5592980224119
AiK312 <zheka824@gmail.com>
2017-10-07 17:02:54 +0300

    final result

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
-
-//module.exports = 
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
   let commonBracketArray = [];//array of bracketsConfig
   let resultBracketArray = [];
   let sizeBracketConfig = bracketsConfig.length;//size bracketsConfig
   let sizeStr = str.length;//size input string
   let count = 0;//main count. If value this count less zer0 => return false
-  let sameBrackets = { countSameSymbols: 0 };//object with property for same brackets and numbers
+  let sameBrackets = { isSame: false };//object with property for same brackets and numbers
   //check for odd size of string.
   if (sizeStr % 2 != 0)
     return false;
@@ -27,39 +18,43 @@ function check(str, bracketsConfig) {
   for (let i = 0; i < sizeStr; i++) {
     if (count == -1)
       return false;
-    if (checkOpenBracket(str[i], str[i + 1], commonBracketArray, sameBrackets)) {
-      resultBracketArray.push(str[i]);
-      count++;
-    }
-    else {
-      if ((commonBracketArray.indexOf(str[i]) - 1) == (commonBracketArray.indexOf(str[i - 1]))) {
+    if (checkOpenBracket(str[i], commonBracketArray, sameBrackets)) {
+      if (sameBrackets.isSame && (str[i] == resultBracketArray[resultBracketArray.length - 1])) {
         resultBracketArray.pop();
         count--;
+        sameBrackets.isSame = false;
+      }
+      else {
+        resultBracketArray.push(str[i]);
+        count++;
+        sameBrackets.isSame = false;
+      }
+    } else {
+      if (commonBracketArray.indexOf(str[i]) - 1 ==
+        commonBracketArray.indexOf(resultBracketArray[resultBracketArray.length - 1])) {
+        resultBracketArray.pop();
+        count--
       } else {
         return false;
       }
     }
-
   }
-
   return true;
 }
 
-console.log(check('()||', config5));
+//console.log(check('111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222', config6));
 
-function checkOpenBracket(symbol, nextSymbol, commonBracketArray) {
-  console.log(commonBracketArray.indexOf(symbol));
+function checkOpenBracket(symbol, commonBracketArray, sameBrackets) {
+  let index = commonBracketArray.indexOf(symbol);
   if (commonBracketArray.indexOf(symbol) % 2 == 0) {
-    if (true) {
-      console.log(symbol);
+    if (symbol == commonBracketArray[index + 1]) {
+      sameBrackets.isSame = true;
+      return true;
     } else {
       return true;
     }
   }
   else {
     return false;
-  }
-
-
+  }  
 }
-
